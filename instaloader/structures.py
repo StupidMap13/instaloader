a87ab8e35fd077e5aa7f9c3cefddc16193a5c416
StupidMap13 <cont@devlife.work>
2026-05-21 14:33:04 +0800

    Add new variables for GraphQL query in structures.py

--- instaloader/structures.py
+++ instaloader/structures.py
@@ -985,13 +985,15 @@ class Profile:
             if not self._has_full_metadata:
                 user_id = self._node.get('id') or self._node.get('pk')
                 variables = {
-                    "id": str(user_id),
-                    "render_surface": "PROFILE",
-                    "__relay_internal__pv__PolarisCannesGuardianExperienceEnabledrelayprovider": True,
-                    "__relay_internal__pv__PolarisCASB976ProfileEnabledrelayprovider": False,
-                    "__relay_internal__pv__PolarisRepostsConsumptionEnabledrelayprovider": False,
-                }
-                data = self._context.doc_id_graphql_query('25980296051578533', variables)
+				    "id": str(user_id),
+				    "render_surface": "PROFILE",
+				    "__relay_internal__pv__PolarisCannesGuardianExperienceEnabledrelayprovider": True,
+				    "__relay_internal__pv__PolarisCASB976ProfileEnabledrelayprovider": False,
+				    "__relay_internal__pv__PolarisRepostsConsumptionEnabledrelayprovider": False,
+				    "__relay_internal__pv__PolarisWebSchoolsEnabledrelayprovider": False,
+				    "enable_integrity_filters": True,
+				}
+				data = self._context.doc_id_graphql_query('27937681195819736', variables)
                 if data is None:
                     raise QueryReturnedNotFoundException('GraphQL query returned None')
                 user_data = data.get('data', {}).get('user')
